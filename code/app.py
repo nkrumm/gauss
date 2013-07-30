047e9a16f4c9bd18bc5681c0bb9b28f3e7de2955
Nik Krumm <nkrumm@gmail.com>
2013-07-30 12:19:13 -0700

    shitload of docstrings

--- code/app.py
+++ code/app.py
@@ -20,17 +20,28 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = 'some_secret'
 
 def event_stream(pubsub):
+    """
+    Unfinished WebSockets streaming functions
+    """
     # TODO: handle client disconnection.
     for message in pubsub.listen():
         yield 'data: %s\n\n' % message['data']
 
 @app.route('/stream')
 def stream():
+    """
+    Flask endpoint to receive streaming events via websockets
+    Not fully implemented.
+    """
     return Response(event_stream(g.pubsub),
                     mimetype="text/event-stream")
 
 @app.before_request
 def before_request():
+    """
+    Open database connections to Mongo and Redis (for python rq)
+    `g` is the flask global variable
+    """
     g.red = redis.StrictRedis()
     g.pubsub = g.red.pubsub()
     g.pubsub.subscribe('chat')
@@ -38,26 +49,44 @@ def before_request():
 
 @app.teardown_request
 def teardown_request(exception):
+    """
+    Close db connection after each flask request
+    Note that db connection pooling is used, so these actually 
+    do stay open for some time.
+    """
     g.conn.close()
 
 @app.route('/')
 def home():
+    """
+    Home Page
+    """
     return render_template("index.html")
 
 @app.route('/studies')
 def studies():
+    """
+    Flask function to list of all available studies
+    """
     study_mgr = study_manager(db="test",conn=g.conn)
     study_rows = study_mgr.get_all_studies()
     return render_template("studies.html", study_rows=study_rows, columns=["study_name","study_description", "date_added"])
 
 @app.route('/studies/<study_name>/')
 def study_info(study_name):
+    """
+    Flask function to show details of a particular study. 
+    Nothing useful currently implemented here.
+    """
     study_mgr = study_manager(db="test",conn=g.conn)
     info = study_mgr.get_study(study_name)
     return render_template("study_info.html", study_name=study_name, data=info)
 
 @app.route('/samples')
 def samples():
+    """
+    Flask function to show list of all samples in the database.
+    """
     mgr = sample_manager(db="test",conn=g.conn)
     rows = mgr.get_all_samples()
     return render_template("samples.html", sample_rows=rows, columns=["sample_id","study_name", "files","attributes"])
@@ -65,6 +94,11 @@ def samples():
 
 @app.route('/samples/<sample_id>')
 def samples_info(sample_id):
+    """
+    Flask function to return sample info page. Returns a summary of 
+    the number of variants in this sample, as well as the files attached
+    to this sample and metadata.
+    """
     mgr = sample_manager(db="test",conn=g.conn)
     var_mgr = variant_manager(db="test",conn=g.conn)
     data = mgr.get_sample(sample_id = sample_id)
@@ -81,12 +115,20 @@ def samples_info(sample_id):
 
 @app.route('/variants/')
 def variants():
+    """
+    Flask function for variants home page. Various stats on the
+    database are returned, not much else. A search box is provided
+    as well.
+    """
     var_mgr = variant_manager(db="test",conn=g.conn)
     stats = var_mgr.get_db_stats()
     return render_template("variants.html", stats=stats)
 
 @app.route('/genotypes/id:<genotype_id>')
 def genotype_details(genotype_id):
+    """
+    Flask function to return genotype info, dashboard, etc.
+    """
     var_mgr = variant_manager(db="test",conn=g.conn)
     data = var_mgr.get_variant(genotype_id)
     if "EFF" in data["annotations"]:
@@ -121,6 +163,10 @@ def genotype_details(genotype_id):
 @app.route('/variants/id:<chrom>:<start>')
 @app.route('/variants/id:<chrom>:<start>-<end>')
 def variant_details(chrom, start, end=None):
+    """
+    Flask function to render the variant-level (not genotype) details
+    Currently returns list of samples with this genotype.
+    """
     var_mgr = variant_manager(db="test",conn=g.conn)
 
     if end is None:
@@ -139,6 +185,10 @@ def variant_details(chrom, start, end=None):
 @app.route('/variants/<chrom>:<start>-<end>')
 @app.route('/samples/<sample_id>/variants')
 def get_variants(gene=None, sample_id=None, chrom=None, start=None, end=None):
+    """
+    Flask function for variants endpoint. Sets up basic query string
+    which is passed in to the javascript datatables AJAX call.
+    """
     var_mgr = variant_manager(db="test",conn=g.conn)
     isoform = None
     if sample_id is not None:
@@ -162,10 +212,45 @@ def get_variants(gene=None, sample_id=None, chrom=None, start=None, end=None):
     return render_template("view_variants.html", title=Markup(title), query_string=Markup(query_string), filters=filters, isoform=isoform)
 
 def is_arg(argname):
+    """
+    Helper function that returns true if argname is in list of GET/POST
+    arguments passed to function by flask. Performs check to ensure
+    that arguments were passed as well
+    """
     return (argname in request.args) and (len(request.args[argname]) > 0)
 
 @app.route('/_variants.json', methods=['GET'])
 def json_variants(return_query=False):
+    """
+    Parses set of query arguments and retrieves records
+    from mongodb. Re-formats some data fields and jenkily adds
+    some HTML formatting to some of them. Returns a json object
+    which is passed to the javascript datatable on the view_variants
+    page. Also creates an appropriate title for the view_variants page.
+
+    This function:
+        - Make basic determination if query is:
+            + for a specific sample
+            + for a specific gene
+            + for a range of chromosome:start-stop
+        - includes/excludes columns based on the `columns` parameter and
+            parses this list to handle dot notation (annotations.EFF...)
+        - Sets a maximum limit of returned rows
+        - parses the include_filters and exclude_filters lists
+        - sets up the mongodb groupby clause if this is requested
+        - Lastly, iterates across the returned raw database records
+            and reformats into a flat n x m table, adding HTML fields 
+            for formatting etc.
+
+    Todo: 
+        - split apart into its own class/API
+        - HTML formatting should not be added here... 
+            (unless clearly asked for in the request)
+        - Better handling of limits/skipby requests
+        - Improve the custom columns parsing (unreadable code right now)
+        - Integrate output options for CSV etc.
+
+    """
     query = defaultdict(lambda: defaultdict(list))
 
     var_mgr = variant_manager(db="test",conn=g.conn)
@@ -370,16 +455,28 @@ def json_variants(return_query=False):
 
 @app.route('/_variants_query.json', methods=['GET'])
 def variant_query_test():
+    """
+    This only returns the mongodb query that would be used
+    to get a list of variants. Testing/debug only.
+    """
     return json_variants(return_query=True)
 
 
 def allowed_file(filename):
+    """
+    Used in uploading files for custom gene/variant sets. 
+    Tests if file extension is part of global ALLOWED_EXTENSIONS.
+    """
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 @app.route('/annotation/dotest')
 @app.route('/annotation/dotest/<n>')
 def do_test_annotation(n=10):
+    """
+    Test function to see if Gauss Workers can do background
+    work via python RQ
+    """
     manager = annotation.GaussWorkerManager(db="test",conn=g.conn)
     worker = annotation.TestWorker()
     #manager.register_worker(worker)
@@ -389,6 +486,11 @@ def do_test_annotation(n=10):
 
 @app.route('/annotation/doGeneAnnotationWorker',methods=['GET', 'POST'])
 def do_GeneAnnotationWorker():
+    """
+    This function starts the GeneAnnotationWorker/Manager combo
+    in order to add annotations for a set of upload genes in the POST
+    variable
+    """
     if request.method == 'POST':
         file_obj = request.files['file']
         filetype = request.form["filetype"]
@@ -414,12 +516,20 @@ def do_GeneAnnotationWorker():
 
 @app.route('/filters')
 def filters():
+    """
+    Show a list of current filters and user-defined/uploaded
+    sets.
+    """
     filter_mgr = filter_manager(db="test",conn=g.conn)
     rows = filter_mgr.get_all_filters()
     return render_template("filters.html", rows=rows, columns=["filter_name","description", "type", "date_added","color"])
 
 @app.route('/filters/delete/<name>')
 def delete_filter(name):
+    """
+    Delete a user-defined set. It is not possible to delete filters
+    defined by GAUSS
+    """
     filter_mgr = filter_manager(db="test",conn=g.conn)
     filter_mgr.delete_filter(name)
     # manager = annotation.GaussWorkerManager(db="test",conn=g.conn)
@@ -431,12 +541,20 @@ def delete_filter(name):
 
 @app.route('/filters.json')
 def jsonfilters():
+    """
+    Returns jsonified list of filters for use in AJAX calls
+    and in modals that display current filters, etc
+    """
     filter_mgr = filter_manager(db="test",conn=g.conn)
     rows = filter_mgr.get_all_filters()
     return jsonify(result=[i for i in rows])
 
 @app.route('/jobs')
 def jobs():
+    """
+    Shows currently queued, running, completed and failed
+    GaussWorker jobs
+    """
     job_mgr = annotation.GaussWorkerManager(db="test",conn=g.conn)
     rows = {}
     rows["queued"] = job_mgr.get_jobs(status="queued")
@@ -448,6 +566,10 @@ def jobs():
 
 @app.route('/uniprot/<refseq_accession>.json')
 def uniprot_record(refseq_accession):
+    """
+    Returns jsonified record from the uniprot database.
+    Used in geneplot drawing for domains, etc.
+    """
     mgr = uniprot_manager(db="uniprot", conn=g.conn)
     data = mgr.get_uniprot_record_by_refseq_id(refseq_accession)
     print data[0]
